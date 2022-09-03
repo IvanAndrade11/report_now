22504722c0fda0eb0843dbdc0bb6e4c2f13e79d7
Ivan Andrade <ivanandrade611@gmail.com>
2022-09-02 19:28:32 -0500

    :sparkles: Add sign-up

--- frontend/utils/js/index.js
+++ frontend/utils/js/index.js
@@ -1,76 +1,68 @@
-$("#signup").click(function() {
-    $("#first").fadeOut("fast", function() {
-    $("#second").fadeIn("fast");
-    });
-    });
-    
-    $("#signin").click(function() {
-    $("#second").fadeOut("fast", function() {
-    $("#first").fadeIn("fast");
-    });
-    });
-    
-    
-      
-             $(function() {
-               $("form[name='login']").validate({
-                 rules: {
-                   
-                   email: {
-                     required: true,
-                     email: true
-                   },
-                   password: {
-                     required: true,
-                     
-                   }
-                 },
-                  messages: {
-                   email: "Please enter a valid email address",
-                  
-                   password: {
-                     required: "Please enter password",
-                    
-                   }
-                   
-                 },
-                 submitHandler: function(form) {
-                   form.submit();
-                 }
-               });
-             });
-             
-    
-    
-    $(function() {
-      
-      $("form[name='registration']").validate({
-        rules: {
-          firstname: "required",
-          lastname: "required",
-          email: {
-            required: true,
-            email: true
-          },
-          password: {
-            required: true,
-            minlength: 5
-          }
-        },
-        
-        messages: {
-          firstname: "Please enter your firstname",
-          lastname: "Please enter your lastname",
-          password: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long"
-          },
-          email: "Please enter a valid email address"
-        },
-      
-        submitHandler: function(form) {
-          form.submit();
-        }
-      });
-    });
-    
+// $("#signin").click(function () {
+//   $("#second").fadeOut("fast", function () {
+//     $("#first").fadeIn("fast");
+//   });
+// });
+
+
+// FORM DE INICIO DE SESIÖN
+$(function () {
+  $("form[name='login']").validate({
+    rules: {
+
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+
+      }
+    },
+    messages: {
+      email: "Por favor ingresa un correo válido",
+
+      password: {
+        required: "Por favor ingresa tu contraseña",
+
+      }
+
+    },
+    submitHandler: function (form) {
+      form.submit();
+    }
+  });
+});
+
+// FORM DE REGISTRO
+$(function () {
+
+  $("form[name='registration']").validate({
+    rules: {
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+
+    messages: {
+      firstname: "Por favor ingrese sus nombres",
+      lastname: "Por favor ingrese sus apellidos",
+      password: {
+        required: "Por favor ingrese su contraseña",
+        minlength: "Tu contraseña debe tener al menos 5 caracteres"
+      },
+      email: "Por favor, introduce una dirección de correo electrónico válida"
+    },
+
+    submitHandler: function (form) {
+      form.submit();
+    }
+  });
+});
