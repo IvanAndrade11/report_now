87e8b7f25b2958d41b27b20b6c6fc9eeca5edd34
Giovanni Baquero <giovancito113@gmail.com>
2022-09-06 19:16:40 -0500

    :sparkles: Creating register page

--- Frontend_V2/src/app/app-routing.module.ts
+++ Frontend_V2/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from '../app/home/home.component'
+import { RegisterComponent } from '../app/register/register.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'registro', component: RegisterComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
