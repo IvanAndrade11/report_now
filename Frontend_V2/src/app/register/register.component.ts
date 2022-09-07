82ad9cecf52669fceaba438695200f1393dd6748
Giovanni Baquero <giovancito113@gmail.com>
2022-09-06 23:02:44 -0500

    :wrench: Validations was modified

--- Frontend_V2/src/app/register/register.component.ts
+++ Frontend_V2/src/app/register/register.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  constructor() {}
+export class RegisterComponent implements OnInit {
+  contactForm!: FormGroup
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.initForm()
+  }
+
+  onSubmit(): void {}
+
+  initForm(): FormGroup {
+    //Declarar las propiedades que tendrá el formulario
+    return this.fb.group({
+      user: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.email, Validators.required]],
+      phone:['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
 }
