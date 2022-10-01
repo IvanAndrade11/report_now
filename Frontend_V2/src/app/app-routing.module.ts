e6b0da90c31c42df7ab26faf99ff791de0f85d97
Giovanni Baquero <giovancito113@gmail.com>
2022-10-01 00:46:04 +0000

    Update 16 files

--- Frontend_V2/src/app/app-routing.module.ts
+++ Frontend_V2/src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { HomeComponent } from '../app/home/home.component'
-import { RegisterComponent } from '../app/register/register.component'
-import { ValidateOtpComponent } from '../app/validate-otp/validate-otp.component'
-import { RestorePasswordComponent } from '../app/restore-password/restore-password.component'
-import { UserHomeComponent } from './user-home/user-home.component'
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "../app/home/home.component";
+import { RegisterComponent } from "../app/register/register.component";
+import { ValidateOtpComponent } from "../app/validate-otp/validate-otp.component";
+import { RestorePasswordComponent } from "../app/restore-password/restore-password.component";
+import { UserHomeComponent } from "./user-home/user-home.component";
+import { NoticeComponent } from "./notice/notice.component";
+import { NewNoticeComponent } from "./new-notice/new-notice.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'registro', component: RegisterComponent },
-  { path: 'validate-otp', component: ValidateOtpComponent },
-  { path: 'restablecer', component: RestorePasswordComponent },
-  { path: 'user-home', component: UserHomeComponent }
-]
+  { path: "", component: HomeComponent },
+  { path: "registro", component: RegisterComponent },
+  { path: "validate-otp", component: ValidateOtpComponent },
+  { path: "restablecer", component: RestorePasswordComponent },
+  { path: "user-home", component: UserHomeComponent },
+  { path: "notice", component: NoticeComponent },
+  { path: "new-notice", component: NewNoticeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
